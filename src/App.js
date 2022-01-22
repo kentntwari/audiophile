68134ef5024e7bedb22840b7ada5fcbd58ffd548
Kent Ntwari <designerjehovah@gmail.com>
2022-01-22 16:24:56 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import Menu from './components/menu';
 import Footer from './components/footer';
 import Home from './pages/home';
 import Motto from './components/Motto';
+import Categories from './pages/categories';
+import Category from './pages/categories/Category';
 
 function App() {
   return (
     <Fragment>
-      <Menu />
       <Router>
+        <Menu />
         <div className="relative px-app App">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/categories" element={<Categories />}>
+              <Route path=":category" element={<Category />} />
+            </Route>
           </Routes>
           <Motto />
         </div>
+        <Footer />
       </Router>
-      <Footer />
     </Fragment>
   );
 }
