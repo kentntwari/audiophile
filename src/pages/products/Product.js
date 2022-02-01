87da190fe6ff6f6bde033c889c5a1fcedb2d7818
Kent Ntwari <designerjehovah@gmail.com>
2022-02-01 16:09:37 -0500

    implemented code splitting with react lazy and react suspense

--- src/pages/products/Product.js
+++ src/pages/products/Product.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { addedToCart } from '../../global/features/cart/cartSlice';
 import useProductData from '../../utilities/hooks/useProductData';
-import ProductInfo from './ProductInfo';
-import Gallery from './Gallery';
-import Prompts from './Prompts';
-import BoxItems from './BoxItems';
-import Features from './Features';
+
+const ProductInfo = lazy(() => import('./ProductInfo'));
+const Gallery = lazy(() => import('./Gallery'));
+const Prompts = lazy(() => import('./Prompts'));
+const BoxItems = lazy(() => import('./BoxItems'));
+const Features = lazy(() => import('./Features'));
 
 const Product = () => {
   let location = useLocation();
@@ -19,40 +20,46 @@ const Product = () => {
   const dispatch = useDispatch();
 
   return (
-    <article className="flex flex-col justify-start items-start gap-22">
-      <article className="grid grid-col place-items-start gap-6">
-        {basicData &&
-          basicData.map(({ image_mobile, slug }) => (
-            <figure key={slug}>
-              <img className="rounded-md" src={image_mobile} alt={slug} />
-            </figure>
-          ))}
+    <Suspense
+      fallback={
+        <div className="w-full h-3/6 flex justify-center items-center">Loading...</div>
+      }>
+      <article className="flex flex-col justify-start items-start gap-22">
+        <article className="grid grid-col place-items-start gap-6">
+          {basicData &&
+            basicData.map(({ image_mobile, slug }) => (
+              <figure key={slug}>
+                <img className="rounded-md" src={image_mobile} alt={slug} />
+              </figure>
+            ))}
 
-        {newProduct === true && <div className="type-overline">new product</div>}
-        {basicData && (
-          <ProductInfo
-            info={basicData}
-            dispatchToStore={dispatch}
-            storeActions={{
-              data: (image, itemName, itemPrice, quantity) =>
-                addedToCart(image, itemName, itemPrice, quantity),
-            }}
-          />
-        )}
+          {newProduct === true && <div className="type-overline">new product</div>}
+
+          {basicData && (
+            <ProductInfo
+              info={basicData}
+              dispatchToStore={dispatch}
+              storeActions={{
+                data: (image, itemName, itemPrice, quantity) =>
+                  addedToCart(image, itemName, itemPrice, quantity),
+              }}
+            />
+          )}
+        </article>
+
+        <section className="grid grid-cols-1 place-items-start gap-22">
+          {basicData && <Features data={basicData} />}
+          <BoxItems data={boxItems} />
+        </section>
+        <Gallery pictures={productGallery} />
+        <section className="flex flex-col gap-10 ">
+          <h2 className="text-center text-[1.75rem] leading-normal tracking-[1px]">
+            You may also like
+          </h2>
+          <Prompts data={prompts} />
+        </section>
       </article>
-
-      <section className="grid grid-cols-1 place-items-start gap-22">
-        {basicData && <Features data={basicData} />}
-        <BoxItems data={boxItems} />
-      </section>
-      <Gallery pictures={productGallery} />
-      <section className="flex flex-col gap-10 ">
-        <h2 className="text-center text-[1.75rem] leading-normal tracking-[1px]">
-          You may also like
-        </h2>
-        <Prompts data={prompts} />
-      </section>
-    </article>
+    </Suspense>
   );
 };
 
