ab16be3fb00b6effd3bf804fae5d033a4ed1bfbb
Kent Ntwari <designerjehovah@gmail.com>
2022-02-02 15:41:11 -0500

    async import of product imge

--- src/pages/home/BuyProductOne.js
+++ src/pages/home/BuyProductOne.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import ButtonWhite from '../../components/buttons/ButtonWhite';
-import zx7Speaker from '../../utilities/images/mobile/image-speaker-zx7.jpg';
 
 const BuyProductOne = () => {
+  const [productImage, setProductImage] = useState(null);
+
+  useEffect(() => {
+    import('../../utilities/images/mobile/image-speaker-zx7.jpg').then((module) =>
+      setProductImage(module.default)
+    );
+  }, []);
+
   return (
     <article className="grid grid-cols-1 grid-rows-1">
       <figure className="col-start-1 row-start-1 row-span-1">
-        <img className="rounded-md" src={zx7Speaker} alt="zx7-speaker" />
+        <img className="rounded-md" src={productImage} alt="zx7-speaker" />
       </figure>
 
       <section className="pl-6 col-start-1 row-start-1 row-span-1">
