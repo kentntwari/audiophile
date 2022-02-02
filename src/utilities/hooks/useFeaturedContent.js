8cae749ce43913f1f9ec6c1b10b46c68fd1c5359
Kent Ntwari <designerjehovah@gmail.com>
2022-02-02 15:46:01 -0500

    refactor custom hook with dynamic import of api endpoitn

--- src/utilities/hooks/useFeaturedContent.js
+++ src/utilities/hooks/useFeaturedContent.js
@@ -1,7 +1,15 @@
 import { useState, useEffect } from 'react';
 import sanityClient from '../../client';
 
-const useFeaturedContent = (endpoint) => {
+const useFeaturedContent = () => {
+  const [apiEndpoint, setApiEndpoint] = useState(null);
+
+  useEffect(() => {
+    import('../../api/endpoints/featuredContent').then((module) =>
+      setApiEndpoint(module.default)
+    );
+  }, []);
+
   const [featuredImage, setFeaturedImage] = useState(() => ({
     mobile: null,
     tablet: null,
@@ -16,42 +24,31 @@ const useFeaturedContent = (endpoint) => {
   useEffect(() => {
     let isFetched = true;
 
-    if (isFetched)
-      return sanityClient
-        .fetch(endpoint)
-        .then(
-          ({
-            image_desktop,
-            image_mobile,
-            image_tablet,
-            product,
-            product_description,
-            slug,
-            category,
-          }) => {
-            setFeaturedImage((prev) => {
-              return {
-                ...prev,
-                mobile: image_mobile,
-                tablet: image_tablet,
-                desktop: image_desktop,
-              };
-            });
+    sanityClient.fetch(apiEndpoint).then((res) => {
+      if (isFetched && res !== null) {
+        setFeaturedImage((prev) => {
+          return {
+            ...prev,
+            mobile: res.image_mobile,
+            tablet: res.image_tablet,
+            desktop: res.image_desktop,
+          };
+        });
 
-            setProductDetails((prev) => {
-              return {
-                ...prev,
-                title: product,
-                description: product_description,
-                slug,
-                category,
-              };
-            });
-          }
-        );
+        setProductDetails((prev) => {
+          return {
+            ...prev,
+            title: res.product,
+            description: res.product_description,
+            slug: res.slug,
+            category: res.category,
+          };
+        });
+      }
+    });
 
     return () => (isFetched = false);
-  }, [endpoint]);
+  }, [apiEndpoint]);
 
   return [featuredImage, productDetails];
 };
