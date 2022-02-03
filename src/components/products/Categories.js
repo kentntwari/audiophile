cf6bb1802b5c02791232b03a3388653956677a7c
Kent Ntwari <designerjehovah@gmail.com>
2022-02-03 00:49:41 -0500

    mempoized categories component

--- src/components/products/Categories.js
+++ src/components/products/Categories.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { memo, useCallback } from 'react';
 import useCategories from '../../utilities/hooks/useCategories';
 import ListCategory from './ListCategory';
 
@@ -23,11 +23,7 @@ const Categories = () => {
     [results]
   );
 
-  return (
-    <section className="grid grid-cols-1 gap-17">
-      {displayCategories()}
-    </section>
-  );
+  return <section className="grid grid-cols-1 gap-17">{displayCategories()}</section>;
 };
 
-export default Categories;
+export default memo(Categories);
