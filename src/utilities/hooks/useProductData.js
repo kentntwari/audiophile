111b987eb63912dc6810a0522f07333768c6afb9
Kent Ntwari <designerjehovah@gmail.com>
2022-02-02 23:13:27 -0500

    refactored hook with async import of api endpoints

--- src/utilities/hooks/useProductData.js
+++ src/utilities/hooks/useProductData.js
@@ -1,93 +1,117 @@
 import { useEffect, useState } from 'react';
 import sanityClient from '../../client';
-import {
-  initialData,
-  sortByCategory,
-  getProductBoxItems,
-  getProductGallery,
-  getMoreProducts,
-} from '../../api/endpoints/product';
 
 const useProductData = (productSlug, productCategory) => {
   /* Get initial info of the poduct:
    image, title,description,price,features, */
+  const [apiEndpoint_general_info, setApiEndpoint_general_info] = useState(null);
   const [generalInfo, setGeneralInfo] = useState(null);
 
-  /* Check if product is latest from its category */
-  const [isNewProduct, setIsNewProduct] = useState(false);
-
-  /* Get boc items */
-  const [boxItems, setBoxItems] = useState(null);
-
-  /* Get product gallery */
-  const [productGallery, setProductGallery] = useState(null);
-
-  /* set prompts to buy other items */
-  const [prompts, setPrompts] = useState(null);
+  useEffect(() => {
+    import('../../api/endpoints/product').then((module) =>
+      setApiEndpoint_general_info(module.initialData)
+    );
+  }, []);
 
   useEffect(() => {
     let isFetched = true;
 
-    if (isFetched)
-      return sanityClient.fetch(initialData).then((res) =>
-        setGeneralInfo(() => {
+    sanityClient.fetch(apiEndpoint_general_info).then((res) => {
+      if (isFetched && res !== null)
+        return setGeneralInfo(() => {
           const filtered = res.filter(({ slug }) => slug === productSlug);
-
           return filtered;
-        })
-      );
+        });
+    });
 
     return () => (isFetched = false);
-  }, [productSlug]);
+  }, [productSlug, apiEndpoint_general_info]);
+
+  /* Check if product is latest from its category */
+  const [apiEndpoint_new_product, setApiEndpoint_new_product] = useState(null);
+  const [isNewProduct, setIsNewProduct] = useState(false);
+
+  useEffect(() => {
+    import('../../api/endpoints/product').then((module) =>
+      setApiEndpoint_new_product(module.sortByCategory(productCategory))
+    );
+  }, [productCategory]);
 
   useEffect(() => {
     let isFetched = true;
 
-    if (isFetched)
-      return sanityClient.fetch(sortByCategory(productCategory)).then((res) =>
-        setIsNewProduct(() => {
+    sanityClient.fetch(apiEndpoint_new_product).then((res) => {
+      if (isFetched && res !== null)
+        return setIsNewProduct(() => {
           return res
             .filter(({ category }) => category === true)
             .slice(0, 1)
             .some(({ slug }) => slug === productSlug);
-        })
-      );
+        });
+    });
 
     return () => (isFetched = false);
-  }, [productSlug, productCategory]);
+  }, [productSlug, apiEndpoint_new_product]);
+
+  /* Get box items */
+  const [apiEndpoint_box_items, setApiEndpoint_box_items] = useState(null);
+  const [boxItems, setBoxItems] = useState(null);
 
   useEffect(() => {
-    let isFetched = true;
-
-    if (isFetched)
-      return sanityClient
-        .fetch(getProductBoxItems(productSlug))
-        .then(({ boxItems }) => setBoxItems(() => boxItems));
-
-    return () => (isFetched = false);
+    import('../../api/endpoints/product').then((module) =>
+      setApiEndpoint_box_items(module.getProductBoxItems(productSlug))
+    );
   }, [productSlug]);
 
   useEffect(() => {
     let isFetched = true;
 
-    if (isFetched)
-      return sanityClient
-        .fetch(getProductGallery(productSlug))
-        .then((res) => setProductGallery(() => res));
+    sanityClient.fetch(apiEndpoint_box_items).then((res) => {
+      if (isFetched && res !== null) return setBoxItems(() => res.boxItems);
+    });
 
     return () => (isFetched = false);
+  }, [apiEndpoint_box_items]);
+
+  /* Get product gallery */
+  const [apiEndPoint_product_gallery, setApiEndpoint_product_gallery] = useState(null);
+  const [productGallery, setProductGallery] = useState(null);
+
+  useEffect(() => {
+    import('../../api/endpoints/product').then((module) =>
+      setApiEndpoint_product_gallery(module.getProductGallery(productSlug))
+    );
   }, [productSlug]);
 
   useEffect(() => {
     let isFetched = true;
 
-    if (isFetched)
-      return sanityClient
-        .fetch(getMoreProducts(productSlug))
-        .then((res) => setPrompts(() => res));
+    sanityClient.fetch(apiEndPoint_product_gallery).then((res) => {
+      if (isFetched && res !== null) return setProductGallery(() => res);
+    });
 
     return () => (isFetched = false);
+  }, [apiEndPoint_product_gallery]);
+
+  /* set prompts to buy other items */
+  const [apiEndpoint_prompts, setApiEndpoint_prompts] = useState(null);
+  const [prompts, setPrompts] = useState(null);
+
+  useEffect(() => {
+    import('../../api/endpoints/product').then((module) =>
+      setApiEndpoint_prompts(module.getMoreProducts(productSlug))
+    );
   }, [productSlug]);
+
+  useEffect(() => {
+    let isFetched = true;
+
+    sanityClient.fetch(apiEndpoint_prompts).then((res) => {
+      if (isFetched && res !== null) return setPrompts(() => res);
+    });
+
+    return () => (isFetched = false);
+  }, [apiEndpoint_prompts]);
 
   return [generalInfo, isNewProduct, boxItems, productGallery, prompts];
 };
