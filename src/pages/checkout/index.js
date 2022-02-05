88e956ec04c32200f2363974aa04b6ea855b82ce
Kent Ntwari <designerjehovah@gmail.com>
2022-02-05 14:05:41 -0500

    checkout component

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Form from './Form';
+import Summary from './Summary';
 
 const Checkout = () => {
-  const location = useLocation();
+  let navigate = useNavigate();
 
-  return <div>{console.log(location)}</div>;
+  return (
+    <Fragment>
+      <section className="mt-5">
+        <p className="opacity-50" onClick={() => navigate(-1)}>
+          Go Back
+        </p>
+      </section>
+
+      <main className="mt-6 flex flex-col gap-8">
+        <Form />
+        <Summary />
+      </main>
+    </Fragment>
+  );
 };
 
 export default Checkout;
