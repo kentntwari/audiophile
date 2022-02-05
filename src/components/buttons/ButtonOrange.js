b7dd9d948097491ec35f97c717447ee370fbd566
Kent Ntwari <designerjehovah@gmail.com>
2022-02-05 13:52:06 -0500

    onClick prop for click evet

--- src/components/buttons/ButtonOrange.js
+++ src/components/buttons/ButtonOrange.js
@@ -2,7 +2,7 @@ import React, { useRef } from 'react';
 
 import useManipulateClasses from '../../utilities/hooks/useManipulateClasses';
 
-const ButtonOrange = ({ children = 'see product', addClass, removeClass, addToCart }) => {
+const ButtonOrange = ({ children = 'see product', addClass, removeClass, onClick }) => {
   const button_orange = useRef();
 
   useManipulateClasses(button_orange, addClass, removeClass);
@@ -12,7 +12,7 @@ const ButtonOrange = ({ children = 'see product', addClass, removeClass, addToCa
       className="bg-orange-full hover:bg-orange-dimmed w-40
          h-12 flex justify-center items-center font-bold text-xsm 
          text-white-full uppercase tracking-style-button"
-      onClick={addToCart}
+      onClick={onClick}
       ref={button_orange}>
       {children}
     </button>
