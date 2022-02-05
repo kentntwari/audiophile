33b82933f536504aa46e51d70d08a99d1f0b6af3
Kent Ntwari <designerjehovah@gmail.com>
2022-02-05 14:02:12 -0500

    toggle cart when visiting checkout page

--- src/components/products/Cart.js
+++ src/components/products/Cart.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useCallback, useRef } from 'react';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link, useLocation } from 'react-router-dom';
+
+import { displayCart } from '../../global/features/cart/showCartSlice';
 import CartItem from './CartItem';
 import ButtonOrange from '../buttons/ButtonOrange';
 
@@ -11,8 +13,12 @@ const Cart = () => {
 
   const cart = useSelector((state) => state.cart);
   const display = useSelector((state) => state.displayCart);
+  const dispatch = useDispatch();
 
+  const cart_div = useRef();
   const cart_popUp = useRef();
+
+  const location = useLocation();
 
   useEffect(() => {
     if (cart.length > 0)
@@ -31,18 +37,28 @@ const Cart = () => {
       );
   }, [cart, cart.length]);
 
+  useEffect(() => {
+    if (
+      location.pathname === '/checkout' &&
+      cart_div.current.classList.contains('block')
+    ) {
+      dispatch(displayCart());
+      return document.body.removeAttribute('class');
+    }
+  }, [location.pathname, dispatch]);
+
   const total = useCallback(() => {
     return cart.reduce((acc, value) => (acc += value.price * value.quantity), 0);
   }, [cart]);
 
   return (
-    <article className={` ${display === true ? 'block' : 'hidden'} w-full`}>
-      <div className="absolute top-0 left-0 w-screen h-screen bg-black-full opacity-50"></div>
+    <article ref={cart_div} className={`${display === true ? 'block' : 'hidden'} w-full`}>
+      <div className="absolute top-0 left-0 z-40 w-screen h-screen bg-black-full opacity-50"></div>
       <dialog
         ref={cart_popUp}
         open
         className="w-[20.43rem] rounded-md px-[28px] py-[33px] absolute top-[1.5rem]
-           left-0 opacity-100 z-40 flex flex-col justify-start items-start gap-8">
+           left-0 opacity-100 z-50 flex flex-col justify-start items-start gap-8">
         <section className="w-full flex justify-between items-start">
           <h6>
             cart <span>({cart.length})</span>
@@ -59,11 +75,13 @@ const Cart = () => {
           <h6 className="tracking-normal">$ {total()}</h6>
         </section>
 
-        <Link to="/checkout" className="w-full" state={{ cart }}>
-          <ButtonOrange addClass={['w-full']} removeClass={['w-40']}>
-            checkout
-          </ButtonOrange>
-        </Link>
+        {cart.length > 0 && (
+          <Link to="/checkout" className="w-full" state={{ cart, total: total() }}>
+            <ButtonOrange addClass={['w-full']} removeClass={['w-40']}>
+              checkout
+            </ButtonOrange>
+          </Link>
+        )}
       </dialog>
     </article>
   );
