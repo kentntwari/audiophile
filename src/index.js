66bc36e18df2568e78d5f5637f2d8315f786e335
Kent Ntwari <designerjehovah@gmail.com>
2022-02-05 13:49:26 -0500

    import browser router from redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import store from './global/store';
@@ -8,7 +9,9 @@ import { Provider } from 'react-redux';
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </React.StrictMode>
   </Provider>,
 
