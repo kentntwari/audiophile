71016364c6ab180e40c878c7f03401f7666c6f1c
Kent Ntwari <designerjehovah@gmail.com>
2022-04-07 16:37:40 -0400

    added scroll prevent when menu is displayed

--- src/components/menu/Navigation.js
+++ src/components/menu/Navigation.js
@@ -1,5 +1,6 @@
-import React, { Fragment, useCallback, createRef } from 'react';
+import React, { Fragment, useCallback, useEffect, createRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
 import MobileNav from './MobileNav';
 import DesktopNav from './DesktopNav';
@@ -10,10 +11,21 @@ const Navigation = () => {
   const isCartDisplayed = useSelector((state) => state.displayCart);
   const dispatch = useDispatch();
 
+  const location = useLocation();
+
   const mobile_menu_ref = createRef();
 
+  useEffect(() => {
+    if (document.body.classList.contains('overflow-y-hidden'))
+      return document.body.classList.remove('overflow-y-hidden');
+  }, [location.pathname]);
+
   const displayMenu = useCallback(() => {
-    if (mobile_menu_ref) return mobile_menu_ref.current.classList.toggle('hidden');
+    if (mobile_menu_ref) {
+      mobile_menu_ref.current.classList.toggle('hidden');
+
+      return document.body.classList.toggle('overflow-y-hidden');
+    }
 
     if (isCartDisplayed) return dispatch(displayCart());
   }, [mobile_menu_ref, isCartDisplayed, dispatch]);
