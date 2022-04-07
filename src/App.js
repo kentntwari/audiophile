e35aa7be303daaf6b597aaeb53683d7f98ce5d91
Kent Ntwari <designerjehovah@gmail.com>
2022-04-07 19:47:54 -0400

    running build

--- src/App.js
+++ src/App.js
@@ -1,53 +1,53 @@
-// import React, { Fragment, lazy, Suspense } from 'react';
-// import { Route, Routes, useLocation } from 'react-router-dom';
+import React, { Fragment, lazy, Suspense } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
 
-// import Menu from './components/menu';
-// import Footer from './components/footer';
+import Menu from './components/menu';
+import Footer from './components/footer';
 
-// const Home = lazy(() => import('./pages/home'));
-// const Categories = lazy(() => import('./pages/categories'));
-// const Category = lazy(() => import('./pages/categories/Category'));
-// const Products = lazy(() => import('./pages/products'));
-// const Product = lazy(() => import('./pages/products/Product'));
-// const Checkout = lazy(() => import('./pages/checkout'));
-// const Cart = lazy(() => import('./components/products/Cart'));
-// const Motto = lazy(() => import('./components/Motto'));
+const Home = lazy(() => import('./pages/home'));
+const Categories = lazy(() => import('./pages/categories'));
+const Category = lazy(() => import('./pages/categories/Category'));
+const Products = lazy(() => import('./pages/products'));
+const Product = lazy(() => import('./pages/products/Product'));
+const Checkout = lazy(() => import('./pages/checkout'));
+const Cart = lazy(() => import('./components/products/Cart'));
+const Motto = lazy(() => import('./components/Motto'));
 
-// function App() {
-//   const location = useLocation();
+function App() {
+  const location = useLocation();
 
-//   return (
-//     <Fragment>
-//       <Menu />
+  return (
+    <Fragment>
+      <Menu />
 
-//       <div className="App overflow-x-hidden">
-//         <Suspense
-//           fallback={
-//             <div className="w-full h-3/2 flex justify-center items-center">
-//               Loading...
-//             </div>
-//           }>
-//           <Cart />
-//           <Routes>
-//             <Route path="/" element={<Home />} />
-//             <Route path="categories" element={<Categories />}>
-//               <Route path=":category" element={<Category />} />
-//             </Route>
+      <div className="App overflow-x-hidden">
+        <Suspense
+          fallback={
+            <div className="w-full h-3/2 flex justify-center items-center">
+              Loading...
+            </div>
+          }>
+          <Cart />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="categories" element={<Categories />}>
+              <Route path=":category" element={<Category />} />
+            </Route>
 
-//             <Route path="products" element={<Products />}>
-//               <Route path=":product" element={<Product />} />
-//             </Route>
+            <Route path="products" element={<Products />}>
+              <Route path=":product" element={<Product />} />
+            </Route>
 
-//             <Route path="checkout" element={<Checkout />} />
-//           </Routes>
+            <Route path="checkout" element={<Checkout />} />
+          </Routes>
           
-//           {location.pathname !== '/checkout' && <Motto />}
-//         </Suspense>
-//       </div>
+          {location.pathname !== '/checkout' && <Motto />}
+        </Suspense>
+      </div>
 
-//       <Footer />
-//     </Fragment>
-//   );
-// }
+      <Footer />
+    </Fragment>
+  );
+}
 
-// export default App;
+export default App;
